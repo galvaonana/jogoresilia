008921b04ac3696664bea0d09764ce0ca959bdca
natalia galvao <nreginagal@gmail.com>
2021-04-14 06:31:16 -0300

    Inicio do jogo, loop principal e ultima linha do jogo

--- scripts/index.js
+++ scripts/index.js
@@ -1,14 +1,8 @@
-function recebiNoZap() {
-    console.log("Recebi a seguinte informação no 'ZapZap': 'Universidade da Ucrânia realiza estudo que comprova que o uso de máscaras faz mal as pessoas' Escolha uma das opções:")
-    let resposta = input(); //input recebido do usuario fica gravado na variavel resposta
-    if (resposta == 1) {
-        acreditarESerBurro;
-        risco++;
-    } else if (resposta == 2) {
-        lerMais();
-        risco--;
-        usoDeMascara++;
+function game() {
+    var status = true;
+    while (status == true) {
+        status = false; // ultima linha do meu jogo
+
     }
 }
-
-recebiNoZap(); //ordem de executar a funcao 'recebiNoZap'
+game();
