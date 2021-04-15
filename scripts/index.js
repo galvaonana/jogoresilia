2543477c6ac7055bddb518de8301d3be93e64154
natalia galvao <nreginagal@gmail.com>
2021-04-14 22:56:36 -0300

    criaçao da home page e de sua funcao principal

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,26 @@
 function game() {
     var status = true;
+    var nome;
+    var score = 0;
+    const arrPersonagens = ["João Cidadão", "Drª Julia", "Capitão Covid"];
+    var player = prompt('0 João Cidadão, 1 Dra Julia ou 2 Capitão Covid?', 'Escolha um número');
+    alert('Olá, eu me chamo ' + arrPersonagens[player]);
+    localStorage.setItem('Personagem', nomeDoPersonagem);
+    var htmlElement = document.getElementById('telajogo');
     while (status == true) {
-        status = false; // ultima linha do meu jogo
-
+        // nome = prompt('Escolha seu personagem');
+        if (nome === "João Cidadão") {
+            alert("Olá João, é um prazer te ver");
+        } else if (nome === "Dra Julia") {
+            alert("Olá Dra Julia, é um prazer te ver");
+        } else if (nome === "Capitão covid") {
+            alert("Olá Capitão Covid, não é um prazer te ver");
+        }
+        status = false;
     }
+
+    htmlElement.innerHTML = "Cuide-se e cuide dos demais"
+    console.log(nome)
 }
+
 game();
